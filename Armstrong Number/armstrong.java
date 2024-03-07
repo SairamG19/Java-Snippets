ec3e6a43dca43a40e099746a2629eb12eba1abcf
Sairam  Gundu <Sairam.gundu192002@gmail.com>
2024-03-08 01:39:16 +0530

    Add files via upload

---
+++ Armstrong Number/armstrong.java
@@ -0,0 +1,31 @@
+class armstrong {
+    public static void main(String[] args) {
+        int no = 370;
+        int t1 = no;
+        int len = 0;
+        while (t1 != 0) {
+
+            len = len + 1;
+            t1 = t1 / 10;
+        }
+        int t2 = no;
+        int arm = 0;
+        int rem;
+        while (t2 != 0) {
+
+            int mul = 1;
+            rem = t2 % 10;
+            for (int i = 1; i <= len; i++) {
+                mul = mul * rem;
+
+            }
+            arm = arm + mul;
+            t2 = t2 / 10;
+        }
+        if (arm == no) {
+            System.out.println(no + "is a armstrong");
+        } else {
+            System.out.println(no + "is not a armstrong");
+        }
+    }
+}
